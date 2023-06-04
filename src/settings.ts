ffbd8d44c4fc68cbf70a84f0e164ccb7dcfc9f8f
Thibaut Mottet <thibaut.mottet@gmail.com>
2023-06-04 19:05:02 +0200

    First commit

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'HomebridgeDenonAVR';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-denon-avr';
+export const PLUGIN_NAME = 'homebridge-denon-avr-1000';
