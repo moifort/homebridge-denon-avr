5ddf3e8d03d2b983f8690260520f238aaa4d70f3
Thibaut Mottet <thibaut.mottet@gmail.com>
2023-06-04 19:53:27 +0200

    First commit

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -22,7 +22,7 @@ export class DenonAccessory {
     this.service.getCharacteristic(this.platform.Characteristic.On)
       .onSet(async (value: CharacteristicValue) => {
         this.state.on = value.valueOf() as boolean;
-        await device.setPower(!this.state.on ? 'ON' : 'STANDBY');
+        await device.setPower(this.state.on ? 'ON' : 'STANDBY');
         await device.setInput('SAT/CBL');
       })
       .onGet(() => this.state.on);
