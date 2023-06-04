941624b562534938bc345f8c00e6648dae8d1af0
Thibaut Mottet <thibaut.mottet@gmail.com>
2023-06-04 18:09:37 +0200

    First commit

---
+++ src/platform.ts
@@ -0,0 +1,44 @@
+import {API, Categories, Characteristic, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service} from 'homebridge';
+
+import {PLATFORM_NAME, PLUGIN_NAME} from './settings';
+import {DenonAccessory} from './platformAccessory';
+
+export class HomebridgeDenonAVR implements DynamicPlatformPlugin {
+  public readonly Service: typeof Service = this.api.hap.Service;
+  public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
+  public readonly accessories: PlatformAccessory[] = [];
+
+  constructor(
+    public readonly log: Logger,
+    public readonly config: PlatformConfig,
+    public readonly api: API,
+  ) {
+    this.log.debug('Finished initializing platform:', this.config.name);
+    this.api.on('didFinishLaunching', () => {
+      log.debug('Executed didFinishLaunching callback');
+      // run the method to discover / register your devices as accessories
+      this.discoverDevices();
+    });
+  }
+
+  configureAccessory(accessory: PlatformAccessory) {
+    this.log.info('Loading accessory from cache:', accessory.displayName);
+    this.accessories.push(accessory);
+  }
+
+  discoverDevices() {
+    const device = { id: '1', name: 'Denon', host: '192.168.0.137' };
+    const uuid = this.api.hap.uuid.generate(device.id);
+    const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+    if (existingAccessory) {
+      this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+      new DenonAccessory(this, existingAccessory);
+    } else {
+      this.log.info('Adding new Denon:', device.id, device.host);
+      const accessory = new this.api.platformAccessory(device.name, uuid, Categories.FAN);
+      accessory.context.device = device;
+      new DenonAccessory(this, accessory);
+      this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+    }
+  }
+}
---
+++ src/platformAccessory.ts
@@ -0,0 +1,36 @@
+import {CharacteristicValue, PlatformAccessory, Service} from 'homebridge';
+import {HomebridgeDenonAVR} from './platform';
+import Denon from 'denon-client';
+
+export class DenonAccessory {
+  private service!: Service;
+  private state = { mute: false };
+
+  constructor(
+    private readonly platform: HomebridgeDenonAVR,
+    private readonly accessory: PlatformAccessory,
+  ) {
+    const device = new Denon.DenonClient(accessory.context.device.host);
+
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Denon')
+      .setCharacteristic(this.platform.Characteristic.Model, 'AVR-X1000')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, accessory.context.device.id);
+
+    this.service = this.accessory.getService(this.platform.Service.Speaker) || this.accessory.addService(this.platform.Service.Speaker);
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
+    this.service.getCharacteristic(this.platform.Characteristic.Mute)
+      .onSet(async (value: CharacteristicValue) => {
+        this.state.mute = value.valueOf() as boolean;
+        await device.setPower(this.state.mute ? 'STANDBY' : 'ON');
+      })
+      .onGet(() => this.state.mute);
+
+    device.on('powerChanged', (data: 'STANDBY' | 'ON') => {
+      this.state.mute = data === 'STANDBY';
+      this.platform.log.info('Update mute', this.state.mute);
+      this.service.updateCharacteristic(this.platform.Characteristic.Mute, this.state.mute);
+    });
+    device.connect();
+  }
+}
